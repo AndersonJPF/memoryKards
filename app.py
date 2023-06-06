145ec364d918035cb698db09ab9f187204d22ff3
AndersonJPF <andersonjpf97@gmail.com>
2023-06-06 11:42:27 -0300

    First version

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cards.db"
+db.init_app(app)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String, unique=True, nullable=False)
+    answer = db.Column(db.String, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    cards = db.session.execute(db.select(Card)).scalars()
+    return render_template("base.html", cards=cards)
+
+@app.post("/add")
+def add_card():
+    q = request.form.get("question")
+    a = request.form.get("answer")
+
+    new_card = Card(question = q, answer = a)
+    db.session.add(new_card)
+    db.session.commit()
+
+    return redirect("/")
+
+@app.get("/delete/<int:card_id>")
+def delete_card(card_id: int):
+    card = db.session.execute(db.select(Card).filter_by(id=card_id)).scalar_one()
+    db.session.delete(card)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/edit/<int:card_id>", methods=["GET", "POST"])
+def edit_card(card_id: int):
+    card = db.session.execute(db.select(Card).filter_by(id=card_id)).scalar_one()
+    if request.method == "POST":
+        card.question = request.form.get("question")
+        card.answer = request.form.get("answer")
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("edit.html", card=card)
+
+
+if __name__ == "__main__":
+    app.run()
